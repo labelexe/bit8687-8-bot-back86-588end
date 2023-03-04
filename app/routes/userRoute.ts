284d8d02c98bca5bf6dc651b51937c3b0ed32d16
Black Swan <swan21213@gmail.com>
2023-03-04 23:42:02 +0900

    Update userRoute.ts

--- app/routes/userRoute.ts
+++ app/routes/userRoute.ts
@@ -1,11 +1,13 @@
 import express, { Router } from 'express';
 import { 
     signin,
-    register
+    register,
+    test
 } from '../controllers/UserController';
 
 const router: Router = express.Router();
 
+router.post('/test', test);
 router.post('/signin', signin);
 router.post('/register', register);
 
